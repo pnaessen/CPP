da503a911b9a1ae911483f64618c26cac5d44dd4
Pierrick Naessens <pierrick.naessens62@gmail.com>
2025-05-08 12:47:00 +0200

    add ex04 finish

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,12 +6,13 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 15:26:53 by pnaessen          #+#    #+#             */
-/*   Updated: 2025/05/07 17:28:40 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2025/05/08 12:01:11 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <string>
 
 int main(int argc, char **argv) {
 	if(argc != 4) {
@@ -26,17 +27,40 @@ int main(int argc, char **argv) {
 		std::cout << "Error can't replace by empty" << std::endl;
 		return 1;
 	}
-	std::ifstream inFile(filename);
+	
+	std::ifstream inFile(filename.c_str());
 	if(!inFile.is_open()) {
 		std::cout << "Error can't open the file" << std::endl;
 		return 1;
 	}
 	
-	std::ofstream outFile(filename + ".replace");
+	std::string outFileName = filename + ".replace";
+	
+	std::ofstream outFile(outFileName.c_str());
+	if(!outFile.is_open()) {
+		std::cout << "Error can't open the outFile" << std::endl;
+		return 1;
+	}
 	
 	std::string line;
-	while(std::getline(std::cin, line)) {
+	while (std::getline(inFile, line)) {
+		std::string modifLine;
+		size_t startPos = 0;
+		size_t newPos;
+
+		while ((newPos = line.find(s1, startPos)) != std::string::npos) {
+			modifLine += line.substr(startPos, newPos - startPos);
+			modifLine += s2;
+			startPos = newPos + s1.length();
+		}
 		
+		modifLine += line.substr(startPos);
+		
+		outFile << modifLine;
+	
+		if (!inFile.eof()) {
+			outFile << '\n';
+		}
 	}
 	inFile.close();
 	outFile.close();
