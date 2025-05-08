efba18de8160ee9fca1220b162491b655c7c805b
Pierrick Naessens <pierrick.naessens62@gmail.com>
2025-05-08 14:54:57 +0200

    update main

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 12:50:03 by pnaessen          #+#    #+#             */
-/*   Updated: 2025/05/08 14:53:21 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2025/05/08 14:54:44 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,12 @@
 int main(int argc, char **argv) {
 	Harl harl;
 	
+	if(argc != 2) {
+		std::cout << "Error Bad arg" << std::endl;
+	}
+	
 	switch 
+		case
 
 	return 0;
 }
