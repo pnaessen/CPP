bd671f65214886a97975d78292cfd0a3cab687ae
Pierrick Naessens <pierrick.naessens62@gmail.com>
2025-05-19 07:25:09 +0200

    update

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 14:05:26 by pnaessen          #+#    #+#             */
-/*   Updated: 2025/05/16 13:32:04 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2025/05/19 07:23:15 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ bool bsp(Point const a, Point const b, Point const c, Point const point) {
 	float areaPAC = std::abs((x1*(y-y3) + x*(y3-y1) + x3*(y1-y))/2.0);
 	float areaPAB = std::abs((x1*(y2-y) + x2*(y-y1) + x*(y1-y2))/2.0);
 	
-	float epsilon = 0.00001;
-	
-	return std::abs(areaABC - (areaPBC + areaPAC + areaPAB)) < epsilon;
+	bool onEdge = (areaPBC == 0 || areaPAC == 0 || areaPAB == 0); 
+	return std::abs(areaABC - (areaPBC + areaPAC + areaPAB)) <= 0 && !onEdge;
 }
