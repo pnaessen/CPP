62d5640b98795089bb72054bbfb5260dd62e0568
Pierrick Naessens <pierrick.naessens62@gmail.com>
2025-05-22 18:04:38 +0200

    fix NateriaSource

--- cpp04/ex03/MateriaSource.cpp
+++ cpp04/ex03/MateriaSource.cpp
@@ -6,17 +6,17 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 16:04:19 by pnaessen          #+#    #+#             */
-/*   Updated: 2025/05/22 17:34:39 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2025/05/22 18:03:22 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MateriaSource.hpp"
 
-MateriaSource::MateriaSource() : IMateriaSource() {
+MateriaSource::MateriaSource()  {
 		std::cout << " MateriaSource constructor call" << std::endl;	
 }
 
-MateriaSource::MateriaSource(const MateriaSource& obj) : IMateriaSource(obj) {
+MateriaSource::MateriaSource(const MateriaSource& obj)  {
 	std::cout << " MateriaSource copy constructor call" << std::endl;	
 
 }
@@ -26,7 +26,7 @@ MateriaSource::~MateriaSource() {
 
 }
 
-MateriaSource& MateriaSource::operator=(MateriaSource& obj) {
+MateriaSource& MateriaSource::operator=(const MateriaSource& obj) {
 	if(this != &obj) {
 		
 	}
--- cpp04/ex03/MateriaSource.hpp
+++ cpp04/ex03/MateriaSource.hpp
@@ -6,7 +6,7 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 15:58:32 by pnaessen          #+#    #+#             */
-/*   Updated: 2025/05/22 16:04:05 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2025/05/22 18:03:53 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,14 @@
 
 	class MateriaSource : public IMateriaSource {
 			private:
-
+				AMateria* _materia[4];
+				int _count;
 			public:
 				MateriaSource();
 				MateriaSource(const MateriaSource& obj);
 				~MateriaSource();
 
-				MateriaSource& operator=(MateriaSource& obj);
+				MateriaSource& operator=(const MateriaSource& obj);
 				void learnMateria(AMateria*);
 				AMateria* createMateria(std::string const& type);
 	};
