dc292de330ee9f6806ddb065fd24bac2d0e7e112
pierrick <pierrick.naessens62@gmail.com>
2025-07-11 10:42:25 +0200

    add cpp07

--- cpp05/ex02/Bureaucrat.hpp
+++ cpp05/ex02/Bureaucrat.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: pn <pn@student.42lyon.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 13:29:29 by pnaessen          #+#    #+#             */
-/*   Updated: 2025/06/06 15:02:25 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2025/07/11 10:36:43 by pn               ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,8 @@
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
 
-// Reset
 #define RESET       "\033[0m"
 
-// Regular colors
 #define BLACK       "\033[30m"
 #define RED         "\033[31m"
 #define GREEN       "\033[32m"
@@ -27,7 +25,6 @@
 #define CYAN        "\033[36m"
 #define WHITE       "\033[37m"
 
-// Bold colors
 #define BOLD_BLACK  "\033[1;30m"
 #define BOLD_RED    "\033[1;31m"
 #define BOLD_GREEN  "\033[1;32m"
@@ -37,7 +34,6 @@
 #define BOLD_CYAN   "\033[1;36m"
 #define BOLD_WHITE  "\033[1;37m"
 
-// Background colors
 #define BG_BLACK    "\033[40m"
 #define BG_RED      "\033[41m"
 #define BG_GREEN    "\033[42m"
---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pn <pn@student.42lyon.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/11 10:32:58 by pn                #+#    #+#             */
+/*   Updated: 2025/07/11 10:33:56 by pn               ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Whatever.hpp"
+
+
+int main( void ) {
+int a = 2;
+int b = 3;
+::swap( a, b );
+std::cout << "a = " << a << ", b = " << b << std::endl;
+std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+std::string c = "chaine1";
+std::string d = "chaine2";
+::swap(c, d);
+std::cout << "c = " << c << ", d = " << d << std::endl;
+std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+std::cout << "max( c,, d ) = " << ::max( c, d ) << std::endl
+
+return 0
+}
