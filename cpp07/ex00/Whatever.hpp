27b80b8fd407908de571bd760862666d82fa2bcd
Pierrick Naessens <pierrick.naessens62@gmail.com>
2025-07-14 13:57:41 +0200

    Add template (google img bad ref and doc)

--- cpp07/ex00/Whatever.hpp
+++ cpp07/ex00/Whatever.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pn <pn@student.42lyon.fr>                  +#+  +:+       +#+        */
+/*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 10:34:01 by pn                #+#    #+#             */
-/*   Updated: 2025/07/11 10:38:47 by pn               ###   ########lyon.fr   */
+/*   Updated: 2025/07/14 13:57:15 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,13 @@
 
 	#include <string>
 	#include <iostream>
+
+	template <typename T>
+	// T swap(s1, s2) {
+	// 	void *tmp = s1;
+	// 	s1 = s2;
+	// 	s2 = tmp;
+
+	// };
 
 #endif
--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pn <pn@student.42lyon.fr>                  +#+  +:+       +#+        */
+/*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 10:32:58 by pn                #+#    #+#             */
-/*   Updated: 2025/07/11 10:33:56 by pn               ###   ########lyon.fr   */
+/*   Updated: 2025/07/14 13:39:30 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 
 int main( void ) {
-int a = 2;
-int b = 3;
-::swap( a, b );
-std::cout << "a = " << a << ", b = " << b << std::endl;
-std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-std::string c = "chaine1";
-std::string d = "chaine2";
-::swap(c, d);
-std::cout << "c = " << c << ", d = " << d << std::endl;
-std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-std::cout << "max( c,, d ) = " << ::max( c, d ) << std::endl
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c,, d ) = " << ::max( c, d ) << std::endl;
 
-return 0
+	return 0;
 }
