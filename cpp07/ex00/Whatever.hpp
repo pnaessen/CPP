9422de8d33e37d65d067ad701aa4408d1605cda4
Pierrick Naessens <pierrick.naessens62@gmail.com>
2025-07-15 13:58:15 +0200

    Finish ex00 first template

--- cpp07/ex00/Whatever.hpp
+++ cpp07/ex00/Whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 10:34:01 by pn                #+#    #+#             */
-/*   Updated: 2025/07/14 13:57:15 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2025/07/15 13:57:34 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,21 @@
 	#include <iostream>
 
 	template <typename T>
-	// T swap(s1, s2) {
-	// 	void *tmp = s1;
-	// 	s1 = s2;
-	// 	s2 = tmp;
+	void swap(T &s1,T &s2) {
+		T tmp = s1;
+		s1 = s2;
+		s2 = tmp;
+	};
+	
+	template <typename T>
+	T max (T &x, T &y) {
+		return ( x >= y ? x : y);
+	};
 
-	// };
+	template <typename T>
+	T  min (T &x, T &y) {
+		return ( x >= y ? y : x);
+	}
+
 
 #endif
