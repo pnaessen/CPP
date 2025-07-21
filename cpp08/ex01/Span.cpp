e3030e49abca316e35daefd7298b5befc001e2ed
Pierrick Naessens <pierrick.naessens62@gmail.com>
2025-07-21 13:20:54 +0200

    Fix : Longest Span and constructor

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 11:53:34 by pnaessen          #+#    #+#             */
-/*   Updated: 2025/07/17 15:35:15 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2025/07/21 09:11:33 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 Span::Span(unsigned int n) : _range(n) {
 	
-	_vec.resize(n);
 }
 
 Span::Span(const Span& obj) : _range(obj._range), _vec(obj._vec) {
@@ -36,12 +35,10 @@ Span& Span::operator=(Span& obj) {
 
 void Span::addNumber(unsigned int n) {
 	
-	return ;
-	if(_vec.capacity() <= _range) {
+	if(_vec.size() >= _range) {
 		throw std::out_of_range("Span is Full");
 	}
 	_vec.push_back(n);
-	return ;
 }
 
 unsigned int Span::shortestSpan() {
@@ -49,7 +46,8 @@ unsigned int Span::shortestSpan() {
 	if(_vec.size() <= 1) {
 		throw std::range_error("No range available");
 	}
-	std::sort(_vec.begin(), _vec.end());
+	std::vector<int> _vecClone = _vec;
+	std::sort(_vecClone.begin(), _vecClone.end());
 	return 1;
 	
 }
@@ -61,7 +59,7 @@ unsigned int Span::longestSpan() {
 	}
 	std::vector<int> _vecClone = _vec;
 	std::sort(_vecClone.begin(), _vecClone.end());
-	std::cout << _vecClone.back() << std::endl;
-	unsigned int longest = (_vecClone.back() - _vecClone.front());
+	int longest = _vecClone.back() - _vecClone.front();
+	
 	return longest;
 }
--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pnaessen <pnaessen@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 11:53:19 by pnaessen          #+#    #+#             */
-/*   Updated: 2025/07/17 15:33:28 by pnaessen         ###   ########lyon.fr   */
+/*   Updated: 2025/07/21 09:08:03 by pnaessen         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int main()
 {
 	Span sp = Span(5);
 	 sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
-	// std::cout << sp.shortestSpan() << std::endl;
-	// std::cout << sp.longestSpan() << std::endl;
+	 sp.addNumber(3);
+	 sp.addNumber(17);
+	 sp.addNumber(9);
+	 sp.addNumber(11);
+	//std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
 	return 0;
 }
